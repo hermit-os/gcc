4640bf4ea532d649c3398dd5ec199238abbad1f4
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2017-05-26 07:50:26 +0200

    create a copy of the environment by its newlib variable

--- libgo/runtime/runtime.c
+++ libgo/runtime/runtime.c
@@ -95,18 +95,29 @@ runtime_goargs(void)
 	args.__capacity = argc;
 }
 
+extern char **environ;
+
 void
 runtime_goenvs_unix(void)
 {
 	String *s;
 	int32 i, n;
 
+#ifdef __hermit__
+	for(n=0; environ[n] != 0; n++)
+		;
+
+	s = runtime_malloc(n*sizeof s[0]);
+	for(i=0; i<n; i++)
+		s[i] = runtime_gostringnocopy(environ[i]);
+#else
 	for(n=0; argv[argc+1+n] != 0; n++)
 		;
 
 	s = runtime_malloc(n*sizeof s[0]);
 	for(i=0; i<n; i++)
 		s[i] = runtime_gostringnocopy(argv[argc+1+i]);
+#endif
 	envs.__values = (void*)s;
 	envs.__count = n;
 	envs.__capacity = n;
