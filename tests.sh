f3889b11b3a853bf92dad8c725bfdab56abbb8be
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2018-05-26 18:05:21 +0200

    fix path to the debian packages

--- tests.sh
+++ tests.sh
@@ -34,7 +34,7 @@ export DEBIAN_FRONTEND="noninteractive"
 apt-get -qq update
 apt-get install -y wget curl gnupg checkinstall gawk dialog apt-utils flex bison binutils texinfo gcc g++ libmpfr-dev libmpc-dev libgmp-dev libisl-dev packaging-dev build-essential libtool autotools-dev autoconf pkg-config apt-transport-https
 
-echo "deb [trusted=yes] https://packagecloud.io/hermitcore/ubuntu vivid main" | tee -a /etc/apt/sources.list 
+echo "deb [trusted=yes] https://dl.bintray.com/hermitcore/ubuntu vivid main" | tee -a /etc/apt/sources.list 
 apt-get update
 apt-get install -y --allow-unauthenticated binutils-hermit
 export PATH=/opt/hermit/bin:$PATH
