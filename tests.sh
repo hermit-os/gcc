33c71a836ee09d787c91ee32f5e28d4c2f4de6b4
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2018-05-27 15:59:29 +0200

    use the target install to install the complete cross compiler

--- tests.sh
+++ tests.sh
@@ -53,7 +53,7 @@ mkdir -p build
 cd build
 ../configure --target=x86_64-hermit --prefix=/opt/hermit --with-newlib --disable-multilib --without-libatomic --with-tune=generic --enable-languages=c,c++,go,fortran,lto --disable-nls --disable-shared --disable-libssp --enable-threads=posix --disable-libgomp --enable-tls --enable-lto --disable-symverss
 make -j2 > build.log
-checkinstall -D -y --exclude=build --pkggroup=main --maintainer=stefan@eonerc.rwth-aachen.de --pkgsource=https://hermitcore.org --pkgname=gcc-hermit --pkgversion=6.3.0 --pkglicense=GPL2 make install-gcc
+checkinstall -D -y --exclude=build --pkggroup=main --maintainer=stefan@eonerc.rwth-aachen.de --pkgsource=https://hermitcore.org --pkgname=gcc-hermit --pkgversion=6.3.0 --pkglicense=GPL2 make install
 
 cd ..
 mkdir -p tmp
