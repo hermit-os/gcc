7bd8028ff193a50de000790cb7bc96ba1cff80e6
Colin Finck <colin.finck@rwth-aachen.de>
2018-07-16 13:57:56 +0200

    Install only required packages and exit early if a build step fails.

--- tests.sh
+++ tests.sh
@@ -31,30 +31,31 @@ else
 
 export DEBIAN_FRONTEND="noninteractive"
 
-apt-get -qq update
-apt-get install -y wget curl gnupg checkinstall gawk dialog apt-utils flex bison binutils texinfo gcc g++ libmpfr-dev libmpc-dev libgmp-dev libisl-dev packaging-dev build-essential libtool autotools-dev autoconf pkg-config apt-transport-https
+apt-get -qq update || exit 1
+apt-get install -y --no-install-recommends bison checkinstall flex gawk gcc g++ libc-dev libgmp-dev libisl-dev libmpc-dev libmpfr-dev libtool texinfo || exit 1
 
-echo "deb [trusted=yes] https://dl.bintray.com/hermitcore/ubuntu bionic main" | tee -a /etc/apt/sources.list 
-apt-get update
-apt-get install -y --allow-unauthenticated binutils-hermit gcc-hermit-bootstrap newlib-hermit-rs pte-hermit-rs libhermit-rs
+echo "deb [trusted=yes] http://dl.bintray.com/hermitcore/ubuntu bionic main" >> /etc/apt/sources.list
+apt-get update || exit 1
+apt-get install -y --allow-unauthenticated binutils-hermit gcc-hermit-bootstrap newlib-hermit-rs pte-hermit-rs || exit 1
+
+# Some GCC configure scripts (e.g. libbacktrace) check whether the C compiler can compile executables, even if it later just compiles a static library.
+# However, our target C compiler cannot build an executable without a libhermit.a present.
+# So we have to install a (possibly older) version of HermitCore-rs for building the C compiler that later compiles HermitCore-rs :)
+apt-get install -y --allow-unauthenticated libhermit-rs
+
 export PATH=/opt/hermit/bin:$PATH
 
-wget ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-0.15.tar.bz2 -O isl-0.15.tar.bz2
-tar jxf isl-0.15.tar.bz2
-mv isl-0.15 isl
-rm isl-0.15.tar.bz2
-
-# don't run fixincludes, this is accidentally done by dh and kinda broken		
-#  reference: https://gcc.gnu.org/ml/gcc/2013-04/msg00171.html		
-#  reference: https://ewontfix.com/12/		
+# don't run fixincludes, this is accidentally done by dh and kinda broken
+#  reference: https://gcc.gnu.org/ml/gcc/2013-04/msg00171.html
+#  reference: https://ewontfix.com/12/
 sed -i 's/STMP_FIXINC=stmp-fixinc/STMP_FIXINC=/g' gcc/configure
 
 mkdir -p build
 cd build
-../configure --target=x86_64-hermit --prefix=/opt/hermit --with-newlib --disable-multilib --without-libatomic --with-tune=generic --enable-languages=c,c++,go,fortran,lto --disable-nls --disable-shared --disable-libssp --enable-threads=posix --disable-libgomp --enable-tls --enable-lto --disable-symvers
-make -j2 > /dev/null
+../configure --target=x86_64-hermit --prefix=/opt/hermit --with-newlib --disable-multilib --without-libatomic --with-tune=generic --enable-languages=c,c++,go,fortran,lto --disable-nls --disable-shared --disable-libssp --enable-threads=posix --disable-libgomp --enable-tls --enable-lto --disable-symvers || exit 1
+make -j2 || exit 1
 apt-get remove -y gcc-hermit-bootstrap
-checkinstall -D -y --exclude=build --pkggroup=main --maintainer=stefan@eonerc.rwth-aachen.de --pkgsource=https://hermitcore.org --pkgname=gcc-hermit-rs --pkgversion=6.3.0 --conflicts=gcc-hermit --pkglicense=GPL2 make install
+checkinstall -D -y --exclude=build --pkggroup=main --maintainer=stefan@eonerc.rwth-aachen.de --pkgsource=https://hermitcore.org --pkgname=gcc-hermit-rs --pkgversion=6.3.0 --conflicts=gcc-hermit --pkglicense=GPL2 make install || exit 1
 
 cd ..
 mkdir -p tmp
