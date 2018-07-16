908038f1dfa9fc19faa13d9c70c8db8630dab12b
Colin Finck <colin.finck@rwth-aachen.de>
2018-07-16 12:18:28 +0200

    Fix gcc-bootstrap build ("make all-gcc" instead of "make"), install only required packages and fail early if a build step fails.

--- tests.sh
+++ tests.sh
@@ -31,29 +31,19 @@ else
 
 export DEBIAN_FRONTEND="noninteractive"
 
-apt-get -qq update
-apt-get install -y wget curl gnupg checkinstall gawk dialog apt-utils flex bison binutils texinfo gcc g++ libmpfr-dev libmpc-dev libgmp-dev libisl-dev packaging-dev build-essential libtool autotools-dev autoconf pkg-config apt-transport-https
+apt-get -qq update || exit 1
+apt-get install -y --no-install-recommends bison checkinstall flex gawk gcc g++ libc-dev libgmp-dev libisl-dev libmpc-dev libmpfr-dev libtool texinfo || exit 1
 
-#echo "deb [trusted=yes] https://dl.bintray.com/hermitcore/ubuntu vivid main" | tee -a /etc/apt/sources.list 
-#apt-get update
-#apt-get install -y --allow-unauthenticated binutils-hermit
-##export PATH=/opt/hermit/bin:$PATH
-
-wget ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-0.15.tar.bz2 -O isl-0.15.tar.bz2
-tar jxf isl-0.15.tar.bz2
-mv isl-0.15 isl
-rm isl-0.15.tar.bz2
-
-# don't run fixincludes, this is accidentally done by dh and kinda broken		
-#  reference: https://gcc.gnu.org/ml/gcc/2013-04/msg00171.html		
-#  reference: https://ewontfix.com/12/		
+# don't run fixincludes, this is accidentally done by dh and kinda broken
+#  reference: https://gcc.gnu.org/ml/gcc/2013-04/msg00171.html
+#  reference: https://ewontfix.com/12/
 sed -i 's/STMP_FIXINC=stmp-fixinc/STMP_FIXINC=/g' gcc/configure
 
 mkdir -p build
 cd build
-../configure --target=x86_64-hermit --prefix=/opt/hermit --without-headers --with-isl --disable-multilib --without-libatomic --with-tune=generic --enable-languages=c,c++,lto --disable-nls --disable-shared --disable-libssp --enable-threads=posix --disable-libgomp --enable-tls --enable-lto --disable-symvers
-make -j2
-checkinstall -D -y --exclude=build --pkggroup=main --maintainer=stefan@eonerc.rwth-aachen.de --pkgsource=https://hermitcore.org --pkgname=gcc-hermit-bootstrap --pkgversion=6.3.0 --pkglicense=GPL2 make install-gcc
+../configure --target=x86_64-hermit --prefix=/opt/hermit --without-headers --with-isl --disable-multilib --without-libatomic --with-tune=generic --enable-languages=c,c++,lto --disable-nls --disable-shared --disable-libssp --enable-threads=posix --disable-libgomp --enable-tls --enable-lto --disable-symvers || exit 1
+make all-gcc -j2 || exit 1
+checkinstall -D -y --exclude=build --pkggroup=main --maintainer=stefan@eonerc.rwth-aachen.de --pkgsource=https://hermitcore.org --pkgname=gcc-hermit-bootstrap --pkgversion=6.3.0 --pkglicense=GPL2 make install-gcc || exit 1
 
 cd ..
 mkdir -p tmp
