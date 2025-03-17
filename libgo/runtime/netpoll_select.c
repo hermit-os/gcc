2a393c7607f349f17e965f89ca0f29a118269003
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2025-03-17 13:33:07 +0100

    e

--- libgo/runtime/netpoll_select.c
+++ libgo/runtime/netpoll_select.c
@@ -59,11 +59,7 @@ runtime_netpollinit(void)
 	int fl;
 
 	FD_ZERO(&fds);
-#ifdef __hermit__
-	allocated = MEMP_NUM_NETCONN;
-#else
 	allocated = 128;
-#endif
 	data = runtime_mallocgc(allocated * sizeof(PollDesc *), 0,
 				FlagNoScan|FlagNoProfiling|FlagNoInvokeGC);
 
