f419e387b67177ec0ee4c20941a238cf44e0ec5d
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2025-07-14 13:53:29 +0200

    Revert "feat: link whole libc"
    This reverts commit 70caee4b1408e99b9e1e40c50e37dd5665d20718.

--- gcc/config/aarch64/hermit.h
+++ gcc/config/aarch64/hermit.h
@@ -51,6 +51,6 @@
 #endif
 
 #undef LIB_SPEC
-#define LIB_SPEC "%{pthread:-lpthread} --whole-archive -lc --no-whole-archive -lg"
+#define LIB_SPEC "%{pthread:-lpthread} -lc -lg"
 
 #endif /* GCC_AARCH64_HERMIT_H */
