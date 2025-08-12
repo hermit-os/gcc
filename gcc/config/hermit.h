e950b222c68d5e836af9a0dc876d0e7cced5e0aa
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2025-08-12 17:14:28 +0200

    fix(gcc): use `%O` instead of `.o` in specs

--- gcc/config/hermit.h
+++ gcc/config/hermit.h
@@ -20,7 +20,7 @@
   } while(0);
 
 #undef  STARTFILE_SPEC
-#define STARTFILE_SPEC "crt0.o%s crti.o%s crtbegin.o%s"
+#define STARTFILE_SPEC "crt0%O%s crti%O%s crtbegin%O%s"
 
 #undef  ENDFILE_SPEC
-#define ENDFILE_SPEC "crtend.o%s crtn.o%s"
+#define ENDFILE_SPEC "crtend%O%s crtn%O%s"
